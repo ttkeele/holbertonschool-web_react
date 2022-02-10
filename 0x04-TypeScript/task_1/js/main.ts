9b7313658b55c29ade7e93c904da52114c1b4f82
ttkeele <3264@holbertonschool.com>
2022-02-10 16:14:13 +0000

    Write an interface named Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,8 @@
-add code
+interface Teacher {
+readonly firstName: string,
+readonly lastName: string,
+fullTimeEmployee: boolean,
+yearsOfExperience?: number,
+location: string,
+[property: string]: any;
+};
