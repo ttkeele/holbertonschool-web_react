18206f2d3b942501e92e8e8e7bb102696b37b743
ttkeele <3264@holbertonschool.com>
2022-02-10 17:26:32 +0000

    first commit, need to finish task 5

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,17 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome = () => 'Working from home';
+  getCoffeeBreak = () => 'Getting a coffee break';
+  workDirectorTasks = () => 'Getting to director tasks';
+}
