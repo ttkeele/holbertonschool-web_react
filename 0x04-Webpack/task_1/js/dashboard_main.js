80c938021a7c33265f563ffed6e6a5039e69eee9
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-02 20:04:19 -0600

    task 1

--- 0x04-Webpack/task_1/js/dashboard_main.js
+++ 0x04-Webpack/task_1/js/dashboard_main.js
@@ -1,16 +1,20 @@
+'use strict';
 import $ from 'jquery';
 import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
 const updateCounter = () => {
-    count = count + 1;
-    $('#count').html(`${count} clicks on the button`);
-}
+  let times = $('#count').html() || 0;
+  $('button').on('click', () => {
+    times++;
+    $('#count').html(`${times} clicks on the button`);
+  });
+};
 
-$('button').on('click', _.debounce(updateCounter, 500));
+_.debounce(updateCounter, 500);
+updateCounter();
