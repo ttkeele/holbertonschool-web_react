61710e4845207fd9a19c51d03547900291e80e3f
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-02 19:48:13 -0600

    task 1

--- 0x04-Webpack/task_1/webpack.config.js
+++ 0x04-Webpack/task_1/webpack.config.js
@@ -2,9 +2,33 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './js/dashboard_main.js',
+    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
     output: {
         path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
+        filename: 'bundle.js'
     },
-};
+    performance: {
+    maxAssetSize: 1000000,
+  },
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      }
+    ]
+  }
+}
