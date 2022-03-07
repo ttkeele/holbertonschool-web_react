1d504365426e650751e750a1f0bef729b3edf1be
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-07 11:20:25 -0600

    app dashboard

--- 0x00-react_intro/task_0/dashboard/src/App.js
+++ 0x00-react_intro/task_0/dashboard/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import holberton_logo from "./holberton_logo.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </>
   );
 }
 
