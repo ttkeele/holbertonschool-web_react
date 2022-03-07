396b81bb2ba4bcf4d9528be0112ef2bcb506d986
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-07 12:02:48 -0600

    not sure where these went
    done now?

--- 0x00-react_intro/task_1/dashboard/src/App.js
+++ 0x00-react_intro/task_1/dashboard/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import holberton_logo from "./holberton_logo.jpg";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+          </p>
       </div>
     </>
   );
