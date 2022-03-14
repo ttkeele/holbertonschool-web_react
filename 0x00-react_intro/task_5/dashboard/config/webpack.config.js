84566f7076089c688ef78c43c6dde30ce6f55dff
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-14 14:23:11 -0500

    ...

--- 0x00-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x00-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,48 +1,54 @@
 const path = require("path");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
+    mode: 'development',
+    entry: './src/index.js',
     devtool: 'inline-source-map',
-	devServer: {
-		hot: true,
-		compress: true,
-	},
+    devServer: {
+        hot: true,
+        compress: true,
+    },
     output: {
-	path: path.resolve('./dist'),
-	filename: 'bundle.js',
-	
+      path: path.resolve('./dist'),
+      filename: 'bundle.js',
     },
     performance: {
-	maxAssetSize: 100000,
-	maxEntrypointSize: 100000,
-	hints: false,
+      maxAssetSize: 100000,
+      maxEntrypointSize: 100000,
+      hints: false,
     },
-    
     module: {
       rules: [
-	{
-	  test: /\.js|\.jsx$/,
-	  exclude: /node_modules/,
-	  loader: "babel-loader",
-	},
-	{
-	  test: /\.css$/i,
-	  use: ["style-loader", "css-loader"],
-	},
-	{
-	  test: /\.(gif|png|jpe?g|svg)$/i,
-	  use: [
-	    "file-loader",
-            {
-	      loader: "image-webpack-loader",
-	      options: {
-		    bypassOnDebug: true, 
-		    disable: true, 
-	      },
-	    },
-          ],
+        {
+          test: /\.js|\.jsx$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
         },
+        {
+          test: /\.css$/i,
+          use: [ "style-loader", "css-loader" ],
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true,
+                disable: true,
+              },
+            },
+          ],
+        }
       ],
     },
-  };
+    // plugins: [
+    //   new CleanWebpackPlugin(),
+    //   new HtmlWebpackPlugin({
+    //     template: path.join(__dirname, "dist", "index.html"),
+    //   }),
+    // ],
+};
