fcc2dc30b86e4693de71090cc7d5b8bb58c51a98
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-14 14:44:55 -0500

    better tests

--- 0x00-react_intro/task_5/dashboard/src/utils/utils.test.js
+++ 0x00-react_intro/task_5/dashboard/src/utils/utils.test.js
@@ -1,22 +1,22 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils"
 describe("getFullYear", () => {
     it("returns correct year", () => {
-	expect(getFullYear()).toEqual(new Date().getFullYear());
+        expect(getFullYear()).toEqual(new Date().getFullYear());
     });
 });
 
 describe("getFooterCopy", () => {
     it("When passed true", () => {
-	expect(getFooterCopy(true)).toEqual("Holberton School");
+        expect(getFooterCopy(true)).toEqual("Holberton School");
     });
 
     it("When passed false", () => {
-	expect(getFooterCopy(false)).toEqual("Holberton School main dashboard");
+        expect(getFooterCopy(false)).toEqual("Holberton School main dashboard");
     });
 });
 
 describe("getLatestNotification", () => {
     it("getting string back", () => {
-	expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
+        expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
     });
 });
