ea2dfb7b0e42b71fa0560ed1f6adc67ab662a629
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-15 21:13:52 -0500

    ...

--- 0x01-react_props/task_5/dashboard/src/Login/Login.js
+++ 0x01-react_props/task_5/dashboard/src/Login/Login.js
@@ -1,17 +1,24 @@
-import React from "react";
-import "./Login.css";
+import logo from '../assets/holberton_logo.jpg';
+import './Login.css';
+import { getFooterCopy, getFullYear } from '../utils/utils.js'
+import React from 'react';
 
 function Login() {
   return (
-    <div className="login">
+    <React.Fragment>
+      <div className='App-body'>
       <p>Login to access the full dashboard</p>
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" />
-      <button>OK</button>
-    </div>
+      <div className='formName'>
+        <label htmlFor='email'>Email: </label>
+        <input name='email' type='email' id='email'></input>
+        <label htmlFor='password'>Password: </label>
+        <input name='password' type='password' id='password'></input>
+        <button>OK</button>
+      </div>
+      </div>
+    </React.Fragment>
   );
 }
+
 
 export default Login;
