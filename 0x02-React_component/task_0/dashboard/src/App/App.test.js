6b6a0cedc6c5fd92e86a19108fc5002b6f5f2d5e
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-15 22:45:59 -0500

    ...

--- 0x02-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x02-React_component/task_0/dashboard/src/App/App.test.js
@@ -1,4 +1,4 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 import React from 'react';
 import "../../config/setupTests";
@@ -6,7 +6,7 @@ import Notifications from '../Notifications/Notifications.js';
 import Header from '../Header/Header.js';
 import Login from '../Login/Login.js';
 import Footer from '../Footer/Footer.js';
-import { expect } from 'chai';
+// import { expect } from 'chai';
 import CourseList from '../CourseList/CourseList';
 
 describe("<App>", () => {
@@ -17,27 +17,27 @@ describe("<App>", () => {
 	});
 
 	it("<App /> is rendered without crashing", () => {
-		expect(wrapper).to.not.be.an('undefined');
+		expect(wrapper.exists()).toBe(true);
 	});
 
     it("<App /> is rendering <Header /> componet", () => {
-        expect(wrapper.contains(<Header />)).to.equal(true);
+        expect(wrapper.contains(<Header />)).toEqual(true);
     });
 
     it("<App /> is rendering <Notification /> componet", () => {
-        expect(wrapper.contains(<Notifications displayDrawer={true}/>)).to.equal(false);
+        expect(wrapper.contains(<Notifications displayDrawer={true}/>)).toEqual(false);
     });
 
     it("<App /> is rendering <Login /> componet", () => {
-        expect(wrapper.contains(<Login />)).to.equal(true);
+        expect(wrapper.contains(<Login />)).toEqual(true);
     });
 
     it("<App /> is rendering <Footer /> componet", () => {
-        expect(wrapper.contains(<Footer />)).to.equal(true);
+        expect(wrapper.contains(<Footer />)).toEqual(true);
     });
 
     it("<App /> is not rending <CourseList /> componet", () => {
-        expect(wrapper.exists(CourseList)).to.equal(false);
+        expect(wrapper.exists(CourseList)).toEqual(false);
     });
 
 })
@@ -50,10 +50,37 @@ describe("<App />", () => {
 	});
 
     it("Tests App when is logged in to see logged in componet", () => {
-        expect(wrapper.exists(Login)).to.equal(false);
+        expect(wrapper.exists(Login)).toEqual(false);
     });
 
     it("Tests App when logged in to see that CourseList exists", () => {
-        expect(wrapper.exists(CourseList)).to.equal(true);
+        expect(wrapper.exists(CourseList)).toEqual(true);
+    })
+})
+
+describe("<App /> ", () => {
+    it("Mocks Alert('Logging you out') when pressing Ctrl + h", () => {
+        let events = {};
+        const myLogOut = jest.fn(() => undefined);
+
+        document.addEventListener = jest.fn((event, cb) => {
+            events[event] = cb;
+        });
+        window.alert = jest.fn();
+        shallow(<App logOut={myLogOut} />);
+
+        events.keydown({ key: "h", ctrlKey: true })
+        expect(myLogOut).toHaveBeenCalled();
+        expect(window.alert).toHaveBeenCalledWith("Logging you out");
+        // const myLogOut = jest.fn(() => undefined);
+
+        // const appComp = mount(<App logOut={myLogOut} />);
+        // const log = jest.spyOn(console, 'log');
+        
+        // expect(appComp.props.logOut);
+        // expect(log);
+    
+        jest.restoreAllMocks();    
+        
     })
 })
