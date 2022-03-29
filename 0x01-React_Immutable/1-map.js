aacc12a51202f0fa12fb5d232f1c11e5a7945a86
ttkeele <85579834+ttkeele@users.noreply.github.com>
2022-03-29 03:17:44 -0500

    fixing

--- 0x01-React_Immutable/1-map.js
+++ 0x01-React_Immutable/1-map.js
@@ -3,13 +3,3 @@ import { Map } from 'immutable';
 export default function getImmutableObject(object) {
   return Map(object);
 }
-
-const object = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-const obj = getImmutableObject(object);
-
-console.log(obj);
